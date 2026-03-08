927026c2516c350973d59a92d7015a6ede10ca9f
ToXiCCuss <rjstdev@outlook.de>
2026-03-08 10:41:38 +0100

    adjust output formatting in `reset_password.sh` script

--- mariadb/reset_password.sh
+++ mariadb/reset_password.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Check if required arguments are provided
 if [ -z "$1" ]; then
   echo "Usage: $0 <username> [host]"
   echo "Example: $1 myuser '%'"
@@ -10,11 +9,12 @@ fi
 NEW_USER=$1
 HOST=${2:-"%"}
 
-# --- AUTOMATISCHE PASSWORT-GENERIERUNG ---
 USER_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c 30)
 
-echo "Resetting MariaDB Password for: $NEW_USER@$HOST"
-echo "New Password:                  $USER_PASS"
+echo "------------------------------------------"
+echo "Resetting Password"
+echo "User: $NEW_USER@$HOST"
+echo "New Password: $USER_PASS"
 echo "------------------------------------------"
 
 mariadb -u root <<EOF
@@ -23,10 +23,9 @@ mariadb -u root <<EOF
 EOF
 
 if [ $? -eq 0 ]; then
-  echo "------------------------------------------"
-  echo "Password for '$NEW_USER'@'$HOST' updated successfully."
-  echo "MAKE SURE TO SAVE THE NEW PASSWORD: $USER_PASS"
+  echo "DONE"
 else
-  echo "Error resetting password."
-  exit 1
+  echo "ERROR"
 fi
+echo "------------------------------------------"
+
